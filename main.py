0f4a7249b42c87b477c464d176ccf46b114541a2
FHP187 <gjimenez@jaumebalmes.net>
2023-02-07 20:40:09 +0100

    1s commit

--- main.py
+++ main.py
@@ -20,18 +20,19 @@ def grafica():
     data = data.groupby(by='NAME').mean('NOTES')
     data = pd.DataFrame(data, columns=['NAME', 'NOTES'])
     colors = ["blue"]
-    plt.bar('', '', width=0.4 , color=colors)
+    name = data.pop("NAME")
+    notes = data.pop("NOTES")
+    plt.bar(notes, name, width=0.4 , color=colors)
     plt.set_ylabel('NOTES')
     plt.set_title('ALUMNAT')
     plt.legend(title='Guillem Jimenez')
     plt.show()
 
 
+
     print('Imprimimos la grafica 2: ')
     data = pd.read_csv("data.csv", usecols=['NAME','GROUP','MODULE','ABSENCES'])
     data = data.dropna()
-
-    plt.bar('', '', width=0.4 , color=colors)
     plt.set_ylabel('Faltes en %')
     plt.set_title('ALUMNAT')
     plt.legend(title='% de faltes DAW2')
